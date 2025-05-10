fe3a718c633a8e10444309f481bf19fa61d9b702
peng <1974724767@qq.com>
2025-05-11 00:18:09 +0800

    Version update

---
+++ data.js
@@ -0,0 +1,140 @@
+window.data = (function () {
+  return {
+    demo_data: [
+      {
+        url: "demo/fgmusic",
+        title: "vue-方格音乐",
+      },
+      {
+        url: "demo/uniapp_shop",
+        title: "uni-app-电商系统",
+      },
+      {
+        url: "demo/vue-shop/dist",
+        title: "vue-电商后台管理系统",
+      },
+      {
+        url: "demo/yang",
+        title: "vue-羊了个羊",
+      },
+      {
+        url: "demo/Minesweeper",
+        title: "vue-扫雷小游戏",
+      },
+      {
+        url: "demo/snake/dist",
+        title: "vue3-贪吃蛇",
+      },
+      {
+        url: "demo/remember",
+        title: "vue-记忆图案",
+      },
+      {
+        url: "demo/white",
+        title: "vue-别踩白方块",
+      },
+      {
+        url: "demo/2048",
+        title: "vue-2048",
+      },
+      {
+        url: "demo/淘宝首页静态/index.html",
+        title: "仿静态淘宝首页",
+      },
+      {
+        url: "demo/fastClick",
+        title: "快速点击",
+      },
+      {
+        url: "demo/bannerSlide",
+        title: "vue-轮播图",
+      },
+      {
+        url: "demo/boe",
+        title: "仿boe官网首页",
+      },
+      {
+        url: "demo/天猫官网demo/login.html",
+        title: "仿天猫登录页",
+      },
+    ],
+    note_data: [
+      {
+        url: "notes",
+        title: "笔记-md",
+      },
+      {
+        url: "note/transformNote.html",
+        title: "CSS-transform",
+      },
+      {
+        url: "note/rotateBox.html",
+        title: "CSS-旋转正方体",
+      },
+      {
+        url: "note/正则表达式应用.html",
+        title: "正则表达式应用",
+      },
+      {
+        url: "note/抽卡.html",
+        title: "CSS-抽卡效果",
+      },
+      {
+        url: "note/拾色板.html",
+        title: "拾色板",
+      },
+      {
+        url: "demo/music",
+        title: "vue-音乐播放器",
+      },
+      {
+        url: "note/同级组件间数据传递.html",
+        title: "vue同级组件间数据传递",
+      },
+      {
+        url: "note/滚动条到达指定位置.html",
+        title: "滚动条到达指定位置",
+      },
+      {
+        url: "note/input验证API.html",
+        title: "input验证API",
+      },
+      {
+        url: "note/控制动画开始暂停.html",
+        title: "js控制动画开始暂停",
+      },
+      {
+        url: "note/自定义右键菜单样式.html",
+        title: "自定义右键菜单样式",
+      },
+      {
+        url: "note/websocket.html",
+        title: "webSocket",
+      },
+      {
+        url: "note/返回顶部.html",
+        title: "js实现返回顶部",
+      },
+      {
+        url: "note/围绕圆形轨迹运动.html",
+        title: "围绕圆形轨迹运动",
+      },
+      {
+        url: "note/简易的拖拽元素.html",
+        title: "简易的拖拽元素",
+      },
+      {
+        url: "note/jquery常用事件.html",
+        title: "Jq常用事件",
+      },
+      {
+        url: "note/太极.html",
+        title: "css实现太极图案",
+      },
+      {
+        url: "note/边框带文字.html",
+        title: "边框带文字fieldset",
+      },
+    ],
+  };
+})();
